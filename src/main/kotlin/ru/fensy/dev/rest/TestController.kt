7d235b0b1ea4018177c35eb3884ad1fff6e146b1
evbogdanov <evgenij.bogdanov.dev@gmail.com>
2025-05-17 01:00:22 +0300

    Отладка раскатки

---
+++ src/main/kotlin/ru/fensy/dev/rest/TestController.kt
@@ -0,0 +1,12 @@
+package ru.fensy.dev.rest
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @GetMapping()
+    fun test() = "hello from Fansy"
+
+}
