077554634c95db6b1ec968af0f06372eacfacc3a
evbogdanov <evbogdanov@inno.tech>
2025-05-20 17:30:33 +0300

    Добавил диррективу с пигинацией

--- src/main/kotlin/ru/fensy/dev/graphql/controller/post/GetPostGraphQLController.kt
+++ src/main/kotlin/ru/fensy/dev/graphql/controller/post/GetPostGraphQLController.kt
@@ -1,6 +1,6 @@
 package ru.fensy.dev.graphql.controller.post
 
-import org.springframework.graphql.data.method.annotation.Argument
+import graphql.schema.DataFetchingEnvironment
 import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
 import ru.fensy.dev.graphql.controller.post.response.PostResponse
@@ -12,6 +12,15 @@ class GetPostGraphQLController(
 ) {
 
     @QueryMapping("getPost")
-    suspend fun getPost(@Argument postId: Long): PostResponse = getPostByIdUseCase.execute(postId)
+    suspend fun getPost(env: DataFetchingEnvironment): PostResponse {
+
+        val postId = (((env.arguments as Map<String, Any>).entries.first().value) as String).toLong()
+        val pageNumber = env.queryDirectives.getImmediateAppliedDirective("page")
+            .firstOrNull()?.getArgument("pageNumber")?.getValue<Int>() ?: 1
+        val pageSize = env.queryDirectives.getImmediateAppliedDirective("page")
+            .firstOrNull()?.getArgument("pageSize")?.getValue<Int>() ?: 25
+
+        return getPostByIdUseCase.execute(postId)
+    }
 
 }
