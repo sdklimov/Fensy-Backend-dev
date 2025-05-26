a086d2cc30c6fc3aef9a1b3315158591633fc0bf
evbogdanov <evbogdanov@inno.tech>
2025-05-26 09:07:46 +0300

    cors config

---
+++ src/main/kotlin/ru/fensy/dev/configuration/security/CorsGlobalConfiguration.kt
@@ -0,0 +1,20 @@
+package ru.fensy.dev.configuration.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.EnableWebFlux
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+@Profile("!local")
+@EnableWebFlux
+class CorsGlobalConfiguration : WebFluxConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("https://fsy.app", "http://fsy.app")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+    }
+}
--- src/main/kotlin/ru/fensy/dev/configuration/security/SecurityConfiguration.kt
+++ src/main/kotlin/ru/fensy/dev/configuration/security/SecurityConfiguration.kt
@@ -2,25 +2,43 @@ package ru.fensy.dev.configuration.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity.AuthorizeExchangeSpec
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
 
 @Configuration(proxyBeanMethods = false)
 @EnableWebFluxSecurity
 class SecurityConfiguration {
 
     @Bean
+    @Profile("!local")
     fun configure(http: ServerHttpSecurity): SecurityWebFilterChain {
         return http
             .csrf { it.disable() }
             .authorizeExchange { auth: AuthorizeExchangeSpec ->
                 auth
-                    .pathMatchers("/test").authenticated()
                     .anyExchange().permitAll()
+            }
+            .oauth2Login(Customizer.withDefaults())
+            .build()
+    }
 
+
+    @Bean
+    @Profile("local")
+    fun configureLocal(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .csrf { it.disable() }
+            .cors {
+                it.disable()
+            }
+            .authorizeExchange { auth: AuthorizeExchangeSpec ->
+                auth.anyExchange().permitAll()
             }
             .oauth2Login(Customizer.withDefaults())
             .build()
