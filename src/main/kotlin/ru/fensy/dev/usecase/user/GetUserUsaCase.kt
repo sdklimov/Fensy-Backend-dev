459e03ee1f11619adbb7968569432d19c6b981a2
evbogdanov <evbogdanov@inno.tech>
2025-06-16 15:42:14 +0300

    Фикс получения пользователя

--- src/main/kotlin/ru/fensy/dev/usecase/user/GetUserUsaCase.kt
+++ src/main/kotlin/ru/fensy/dev/usecase/user/GetUserUsaCase.kt
@@ -2,8 +2,11 @@ package ru.fensy.dev.usecase.user
 
 import graphql.schema.DataFetchingEnvironment
 import org.springframework.stereotype.Component
+import ru.fensy.dev.graphql.controller.post.response.PostResponse
 import ru.fensy.dev.graphql.controller.user.response.UserResponse
 import ru.fensy.dev.repository.UserRepository
+import ru.fensy.dev.usecase.BaseUseCase
+import ru.fensy.dev.usecase.post.operationmodel.CreatePostOperationRq
 
 /**
  * Получить пользователя
@@ -11,7 +14,7 @@ import ru.fensy.dev.repository.UserRepository
 @Component
 class GetUserUsaCase(
     private val userRepository: UserRepository,
-) {
+): BaseUseCase() {
 
     suspend fun execute(env: DataFetchingEnvironment): UserResponse {
         val username = env.getArgument<String>("username")
@@ -20,7 +23,7 @@ class GetUserUsaCase(
             val user = userRepository.findByUsername(it)
             UserResponse(user = user, message = "Пользователь получен успешно", success = true)
 
-        } ?: UserResponse(user = null, message = "Пользователь не найден", success = false)
+        } ?: UserResponse(user = currentUser(required = true), success = true, message = "Пользователь получен успешно")
 
     }
 
